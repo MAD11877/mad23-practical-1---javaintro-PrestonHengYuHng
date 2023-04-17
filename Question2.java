0d8ed511dafdd17df8a96be5ada563c3844ef5f0
PrestonHengYuHng <130948648+PrestonHengYuHng@users.noreply.github.com>
2023-04-17 11:21:27 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,13 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter height: ");
     double height = in.nextDouble();
+    System.out.print("Enter weight: ");
+    double weight = in.nextDouble();
+    
+    double bmi = weight/ (height * height);
+
+    System.out.println(bmi);
   }
 }
